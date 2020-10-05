22797f23e61e5f1e42a5dacfc4cc86e870329e73
ibnvladimir <49831763+ibnvladimir@users.noreply.github.com>
2020-10-05 13:32:43 +0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+  	if (num < 0){
+		num *= - 1;
+	}
+	
+	let result = String(num);
+	result = Array.from(result);
+	result = result.reverse().join('');
+	
+	return parseInt(result);
 }
